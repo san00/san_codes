d1de810d59019371575fd0265e77b3bcb597fceb
sanscomp <sanscomp@anslems-iMac.lan>
2020-11-16 20:24:00 +0000

    Add fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,14 +29,25 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // background_color: `#663399`,
+        // theme_color: `#663399`,
+        // display: `minimal-ui`,
+        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
-   
-
+    plugins: [
+      {
+        resolve: `gatsby-plugin-google-fonts`,
+        options: {
+          fonts: [
+            `Quicksand`,
+            'Mukta Vaani'
+            // you can also specify font weights and styles
+          ],
+          display: 'swap'
+        }
+      }
+    ]
+  },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
