e34232905ffea84d07122c4420873397c6894b6b
sanscomp <sanscomp@anslems-iMac.lan>
2020-11-17 13:46:44 +0000

    Add placeholder text to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,25 @@
 import React from "react"
-import AboutMe from "../components/aboutMe"
+
+import Layout from "../components/layout"
+import Container from "../styles/global/container"
+import { AboutText, AboutLink } from "../styles/components/about"
 
 function About() {
   return (
-    <div>
-      <AboutMe />
-    </div>
+    <Layout>
+      <Container>
+        <AboutText>
+          Hello! I'm currently working on this page. <br /> For now, heres a
+          random dad joke:
+        </AboutText>
+        <AboutText>
+          What did one plate say to the other plate? <br /> Dinner is on me!
+        </AboutText>
+        <AboutLink to="/">
+          Go back to the home page. It's currently nicer there
+        </AboutLink>
+      </Container>
+    </Layout>
   )
 }
 
