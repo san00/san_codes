cba3f82d430882fad8ff6d31435abc6361ead586
sanscomp <sanscomp@anslems-iMac.lan>
2020-11-17 13:46:05 +0000

    Remove nav links

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import HeaderContainer, {
   HeaderNavAlignRight,
-  HeaderNavAlignLeft,
   HeaderLink,
 } from "../styles/components/header"
 import Container from "../styles/global/container"
@@ -14,10 +13,6 @@ const Header = () => {
         <HeaderNavAlignRight>
           <HeaderLink to="/about">About</HeaderLink>
         </HeaderNavAlignRight>
-        <HeaderNavAlignLeft>
-          <HeaderLink to="/projects">Projects</HeaderLink>
-        </HeaderNavAlignLeft>
-        <HeaderLink to="/blog">Blog</HeaderLink>
       </HeaderContainer>
     </Container>
   )
