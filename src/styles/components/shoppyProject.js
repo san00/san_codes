f652965e6a3f673f049626e097e45f2d07916591
sanscomp <sanscomp@anslems-iMac.lan>
2020-11-23 19:57:19 +0000

    Add queries & icons to shoppy project

--- src/styles/components/shoppyProject.js
+++ src/styles/components/shoppyProject.js
@@ -1,12 +1,17 @@
 import styled from "styled-components"
-import { Neutral, spacingScale, typeScale } from "../utils"
+import { Neutral, spacingScale, mediaQuery, typeScale } from "../utils"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
 
 const ShoppyContainer = styled.section`
   display: flex;
-  flex-direction: row;
-  margin: ${spacingScale.spacing_xl};
+  flex-direction: column;
+  padding-left: calc((100vw - 60em) / 2);
+  padding-right: calc((100vw - 60em) / 2);
+
+  @media (min-width: ${mediaQuery.breakpoint1}) {
+    flex-direction: row;
+  }
 `
 
 export const InnerWrapper = styled.section`
@@ -14,16 +19,19 @@ export const InnerWrapper = styled.section`
   flex-direction: column;
 `
 export const ShoppyLink = styled(Link)`
-  text-decoration: none; ;
+  text-decoration: none;
 `
 
 export const ShoppyImg = styled(Img)`
-  width: 100%;
   height: 100%;
-  margin-right: ${spacingScale.spacing_xl};
+
+  @media (min-width: ${mediaQuery.breakpoint1}) {
+    margin-right: ${spacingScale.spacing_xl};
+    min-width: 50%;
+  }
 `
 export const ShoppyHeader = styled.h3`
-  font-size: 2.441rem;
+  font-size: 1.563rem;
   line-height: 1.28em;
   padding-left: ${spacingScale.spacing_l};
   font-family: "Quicksand", sans-serif;
@@ -32,13 +40,33 @@ export const ShoppyHeader = styled.h3`
 `
 
 export const ShoppyText = styled.p`
-  font-size:${typeScale.paragraph}; 
+  font-size: ${typeScale.paragraph};
   line-height: 1.58em;
   word-break: normal;
   padding-left: ${spacingScale.spacing_l};
   font-family: "Mukta Vaani", sans-serif;
   font-weight: 500;
   color: ${Neutral[500]};
+`
+
+export const IconWrapper = styled.section`
+  display: flex;
+  margin-left: auto;
+  font-size: ${typeScale.paragraph};
+  line-height: 1.58em;
+  padding-top: ${spacingScale.spacing_l};
+  font-family: "Mukta Vaani", sans-serif;
+`
+
+export const Github = styled.a`
+  fill: ${Neutral[400]};
+  color: ${Neutral[400]};
+  margin-left: auto;
+  & :hover {
+    fill: ${Neutral[600]};
+    color: ${Neutral[600]};
+    transition: all 0.1s ease;
+  }
 `
 
 export default ShoppyContainer
