414627ea500a0412fa5ca13d4254177c5694c07d
san00 <you@example.com>
2021-02-01 16:54:38 +0000

    Update bio info

--- src/styles/components/aboutMe.js
+++ src/styles/components/aboutMe.js
@@ -1,5 +1,6 @@
 import styled from "styled-components"
-import { spacingScale, Neutral, mediaQuery } from "../utils"
+import { spacingScale, Neutral, Red, mediaQuery } from "../utils"
+import { Link } from "gatsby"
 
 export const AboutMeText = styled.h1`
   color: ${Neutral[600]};
@@ -9,9 +10,9 @@ export const AboutMeText = styled.h1`
   font-family: "Quicksand", sans-serif;
   font-weight: 500;
   line-height: 1.28em;
-  text-align:center;
+  text-align: center;
   @media (min-width: ${mediaQuery.breakpoint1}) {
-   text-align:left;
+    text-align: left;
   }
 `
 
@@ -20,13 +21,13 @@ export const Text = styled.p`
   color: ${Neutral[600]};
   word-break: normal;
   font-size: 1.25rem;
-  text-align:center;
+  text-align: center;
   line-height: 1.58em;
   font-family: "Quicksand", sans-serif;
   padding-left: ${spacingScale.spacing_xs};
   margin: 0;
   @media (min-width: ${mediaQuery.breakpoint1}) {
-   text-align:left;
+    text-align: left;
   }
 `
 
@@ -36,9 +37,64 @@ export const BioText = styled.p`
   line-height: 1.58em;
   color: ${Neutral[600]};
   margin-left: ${spacingScale.spacing_xl};
-  margin-right: ${spacingScale.spacing_xl}; 
+  margin-right: ${spacingScale.spacing_xl};
   word-break: normal;
   padding: ${spacingScale.spacing_m};
   font-family: "Quicksand", sans-serif;
   font-weight: 300;
+`
+export const BioLink = styled(Link)`
+  padding: ${spacingScale.spacing_xs};
+  text-align: center;
+  color: ${Red[100]};
+`
+
+export const Wrap = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+`
+export const GoalsHeader = styled.p`
+  text-align: center;
+  font-size: 1.25rem;
+  line-height: 1.58em;
+  color: ${Neutral[600]};
+  margin-left: ${spacingScale.spacing_xl};
+  margin-right: ${spacingScale.spacing_xl};
+  word-break: normal;
+  padding: ${spacingScale.spacing_m};
+  font-family: "Quicksand", sans-serif;
+  font-weight: 700;
+  margin: 0;
+  padding: 0;
+`
+export const Goals = styled.p`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  font-size: 1.25rem;
+  line-height: 1.58em;
+  color: ${Neutral[600]};
+  margin-left: ${spacingScale.spacing_xl};
+  margin-right: ${spacingScale.spacing_xl};
+  word-break: normal;
+  padding: ${spacingScale.spacing_m};
+  font-family: "Quicksand", sans-serif;
+  font-weight: 300;
+  margin: 0;
+  padding: 0;
+`
+
+export const Twitter = styled.a`
+  text-align: center;
+  font-size: 1.25rem;
+  line-height: 1.58em;
+  color: ${Neutral[600]};
+  padding: ${spacingScale.spacing_xs};
+  font-family: "Quicksand", sans-serif;
+  font-weight: 300;
+  &:hover {
+    color: ${Red[100]};
+  }
 `
